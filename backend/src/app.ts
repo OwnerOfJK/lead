a3790eeb4fdbd0e05d1c64d1125eecb0932ef2f0
OwnerOfJK <john.kaller@hotmail.de>
2026-02-09 22:39:36 +0100

    Add new routers to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,9 @@ import cors from "cors";
 import { config } from "./config";
 import { errorHandler } from "./middleware/errorHandler";
 import { authRouter } from "./modules/auth/auth.routes";
+import { connectionsRouter } from "./modules/connections/connections.routes";
+import { contactsRouter } from "./modules/contacts/contacts.routes";
+import "./providers";
 import "./types";
 
 const app = express();
@@ -15,6 +18,8 @@ app.get("/health", (_req, res) => {
 });
 
 app.use("/auth", authRouter);
+app.use("/connections", connectionsRouter);
+app.use("/contacts", contactsRouter);
 
 app.use(errorHandler);
 
