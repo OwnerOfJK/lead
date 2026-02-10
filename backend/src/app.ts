c1bbc2f673a70c3ba0039bdbd1175aaf16331700
OwnerOfJK <john.kaller@hotmail.de>
2026-02-10 15:43:07 +0100

    Add chat route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import { errorHandler } from "./middleware/errorHandler";
 import { authRouter } from "./modules/auth/auth.routes";
 import { connectionsRouter } from "./modules/connections/connections.routes";
 import { contactsRouter } from "./modules/contacts/contacts.routes";
+import { chatRouter } from "./modules/chat/chat.routes";
 import "./providers";
 import "./types";
 
@@ -20,6 +21,7 @@ app.get("/health", (_req, res) => {
 app.use("/auth", authRouter);
 app.use("/connections", connectionsRouter);
 app.use("/contacts", contactsRouter);
+app.use("/chat", chatRouter);
 
 app.use(errorHandler);
 
